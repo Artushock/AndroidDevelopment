04592d946eb202873b9f81ce06564c321c4b848e
Artiom Ponomariov <nyuar@ukr.net>
2021-05-13 09:15:27 +0300

    2. Add all learned variables

--- src/main/java/java_core_for_android/lesson1_introduction/HomeWorkLesson1.java
+++ src/main/java/java_core_for_android/lesson1_introduction/HomeWorkLesson1.java
@@ -5,6 +5,21 @@ package java_core_for_android.lesson1_introduction;
 
 public class HomeWorkLesson1 {
     public static void main(String[] args) {
+        // 2.
+        byte byteVar = 120;
+        short shortVar = 10000;
+        int intVar = 123;
+        long longVar = 1000000L;
+
+        float floatVar = 3.14f;
+        double doubleVar = 123.456;
+
+        char charVar = 'c';
+
+        boolean isItTrue = false;
+
+        String strVar = "Geek Brains!";
+
         
     }
 }
