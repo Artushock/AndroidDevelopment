24c77dc6cc0bc49e54a332fa9696249e373263cc
Artiom Ponomariov <nyuar@ukr.net>
2021-05-13 09:05:21 +0300

    Initial commit

---
+++ src/main/java/java_core_for_android/lesson1_introduction/HomeWorkLesson1.java
@@ -0,0 +1,10 @@
+package java_core_for_android.lesson1_introduction;
+/*
+    @author Artiom Ponomariov
+*/
+
+public class HomeWorkLesson1 {
+    public static void main(String[] args) {
+        
+    }
+}
