19db891942590a49f7f8d5a39c548da79bc24ff0
Artiom Ponomariov <artushock@gmail.com>
2021-05-16 16:32:39 +0300

    added HomeWork_2_6 class

---
+++ src/main/java/java_core_for_android/lesson2_main_constructions/HomeWork_2_6.java
@@ -0,0 +1,30 @@
+package java_core_for_android.lesson2_main_constructions;/*
+    @author Artiom Ponomariov
+*/
+
+public class HomeWork_2_6 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 1, 2, 3, 3};
+
+        boolean isBalanced = checkBalance(arr);
+        System.out.println(isBalanced);
+    }
+
+    private static boolean checkBalance(int[] arr) {
+        int sum = 0;
+        for (int n : arr) {
+            sum +=n;
+        }
+
+        if (sum % 2 != 0) return false;
+
+        sum = sum/2;
+        for (int n : arr){
+            sum = sum - n;
+            if (sum <= 0) break;
+        }
+        return sum == 0;
+    }
+}
