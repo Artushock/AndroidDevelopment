e7cea897be911140516dd122f740db48e647d90d
Artiom Ponomariov <artushock@gmail.com>
2021-05-16 15:27:21 +0300

    added HomeWork_2_2 class

--- src/main/java/java_core_for_android/lesson2_main_constructions/HomeWorkLesson2.java
+++
@@ -1,6 +0,0 @@
-package java_core_for_android.lesson2_main_constructions;/*
-    @author Artiom Ponomariov
-*/
-
-public class HomeWorkLesson2 {
-}
---
+++ src/main/java/java_core_for_android/lesson2_main_constructions/HomeWork_2_2.java
@@ -0,0 +1,21 @@
+package java_core_for_android.lesson2_main_constructions;
+/*
+    @author Artiom Ponomariov
+*/
+
+public class HomeWork_2_2 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[8];
+        int arg = 0;
+
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i] = arg;
+            arg+=3;
+        }
+
+        for (int n : arr){
+            System.out.print(n + " ");
+        }
+    }
+}
